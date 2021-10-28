89e738271c14ef8734f0bb21c53327d7a7085b27
qrulleau <quentin.rulleau@yahoo.com>
2021-10-28 16:41:41 +0200

    Add : tab fonctionnality for experience section

--- script/main.js
+++ script/main.js
@@ -2,30 +2,50 @@ const buttonModal = document.querySelector('.modal-button');
 const modal = document.querySelector('.modal');
 const closeModal = document.querySelector('.fa-times-circle');
 
-const buttonSelector = document.querySelectorAll('.education-choice');
-console.log(buttonSelector);
-
-let test = document.querySelector('.btec').value;
-//console.log(test);
+const buttonSelectorEducation = document.querySelectorAll('.education-choice');
+const buttonSelectorExperience = document.querySelectorAll('.sf');
 
 buttonModal.addEventListener('click', function () {
-  modal.classList.add('display');
+	modal.classList.add('display');
 });
-
 closeModal.addEventListener('click', function () {
-  modal.classList.remove('display');
+	modal.classList.remove('display');
 });
 
-buttonSelector.forEach((btn) => {
-  // let reactElement = document.querySelector(btn.value);
-  //console.log(reactElement);
+buttonSelectorEducation.forEach((btn) => {
+	btn.addEventListener('click', () => {
+		let activeElement = document.querySelector('.active-education');
+		let contents = document.querySelectorAll('.job');
 
-  btn.addEventListener('click', () => {
-    let activeElement = document.querySelector('.active-education');
-    activeElement.classList.remove('active-education');
-    console.log(btn);
-    console.log(activeElement.value);
-    console.log(querySelector(activeElement.value));
-    btn.classList.add('active-education');
-  });
+		activeElement.classList.remove('active-education');
+		btn.classList.add('active-education');
+
+		contents.forEach((content) => {
+			content.classList.remove('active');
+		});
+
+		const value = btn.value;
+		const DisplayContent = document.getElementById(value);
+
+		DisplayContent.classList.add('active');
+	});
+});
+
+buttonSelectorExperience.forEach((btn) => {
+	btn.addEventListener('click', () => {
+		let activeElement = document.querySelector('.active-experience');
+		let contents = document.querySelectorAll('.compagny');
+
+		activeElement.classList.remove('active-experience');
+		btn.classList.add('active-experience');
+
+		contents.forEach((content) => {
+			content.classList.remove('active');
+		});
+
+		const value = btn.value;
+		const DisplayContent = document.getElementById(value);
+
+		DisplayContent.classList.add('active');
+	});
 });
